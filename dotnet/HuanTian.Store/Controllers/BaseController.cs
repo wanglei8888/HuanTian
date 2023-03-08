a8be229170b6a45d1ef83cb22f0d19138cad7d3f
wanglei <271976304@qq.com>
2023-03-08 17:00:30 +0800

    fix:修复不能ctrl + shift + F 全局查询的错误

---
+++ dotnet/HuanTian.Store/Controllers/BaseController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Net;
+using System.Text;
+using HuanTian.Common;
+using NPOI.SS.Formula.Functions;
+
+namespace Huangtian.Store.Controllers
+{
+    public class BaseController : Controller
+    {
+        /// <summary>
+        /// 对所有API返回的结果截取
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var myResult = (ObjectResult)filterContext.Result ?? new ObjectResult("");
+            var value = myResult.Value == null ? "" : myResult.Value;
+            // 返回的类型不是APIResult就对结果进行修改
+            if (value.GetType().Name != "APIResult")
+            {
+                var resultModels = new APIResult
+                {
+                    Result = value,
+                    Status = true,
+                    Message = "",
+                    Code = ResultCodeEnum.Success
+                };
+                filterContext.Result = Ok(resultModels);
+            }
+
+            Response.Headers.Add("Custom-Header", Guid.NewGuid().ToString());
+            base.OnActionExecuted(filterContext);
+        }
+
+        //byte[] result;
+        //filterContext.HttpContext.Session.TryGetValue("UserID", out result);
+        //if (result == null)
+        //{
+        //    filterContext.Result = new RedirectResult("/Login/Index");
+        //    return;
+        //}
+    }
+}
