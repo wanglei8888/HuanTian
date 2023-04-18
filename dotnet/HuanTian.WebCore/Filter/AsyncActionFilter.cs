b8eb9745564602ccf3ace0bf2aec7d73b8723f7a
wanglei <271976304@qq.com>
2023-04-18 23:35:02 +0800

    feat:新增操作过滤器

---
+++ dotnet/HuanTian.WebCore/Filter/AsyncActionFilter.cs
@@ -0,0 +1,48 @@
+﻿#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+ * 版权所有 (c) 2023  NJRN 保留所有权利。
+ * CLR版本：4.0.30319.42000
+ * 机器名称：DESKTOP-4P8G8RH
+ * 公司名称：
+ * 命名空间：HuanTian.WebCore.Filter
+ * 唯一标识：7f0fbe36-3390-4781-907f-989f63ef0514
+ * 文件名：testclass
+ * 当前用户域：DESKTOP-4P8G8RH
+ * 
+ * 创建者：wanglei
+ * 电子邮箱：271976304@qq.com
+ * 创建时间：2023/4/18 20:42:38
+ * 版本：V1.0.0
+ * 描述：
+ *
+ * ----------------------------------------------------------------
+ * 修改人：
+ * 时间：
+ * 修改说明：
+ *
+ * 版本：V1.0.1
+ *----------------------------------------------------------------*/
+#endregion << 版 本 注 释 >>
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuanTian.WebCore.Filter
+{
+    /// <summary>
+    /// 操作过滤器
+    /// </summary>
+    public class AsyncActionFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            // 调用下一个中间件或过滤器
+            var result = await next();
+
+        }
+    }
+}
